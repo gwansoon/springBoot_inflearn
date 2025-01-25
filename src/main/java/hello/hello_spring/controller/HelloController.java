d7ac50cf4df5a7ebb0982fa7dfe032be0e965ab5
gwansoon <zx6849@naver.com>
2025-01-26 03:01:14 +0900

    @ResponseBody = json이 기본

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,8 +19,32 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
-
-
     }
 
+    @GetMapping("hello-string")
+    @ResponseBody  // http의 body 부의 데이이터를 직접 넣어주겠다는 뜻 view를 안하고 그대로 내려준다
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody // json 방식으로 출력됌 , 최근 추세 , 없으면 그냥 html 파일 출력시켜줌
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+
+        return hello;
+    }
+
+    static class Hello { //객체
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
